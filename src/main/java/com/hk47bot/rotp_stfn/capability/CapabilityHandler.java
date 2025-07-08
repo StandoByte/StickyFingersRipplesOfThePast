448f393e830be8279c69aec552692124a745c56f
Yarost228 <yarik.alferov1@gmail.com>
2025-07-08 19:20:08 +0300

    Removed ZipperData

--- src/main/java/com/hk47bot/rotp_stfn/capability/CapabilityHandler.java
+++ src/main/java/com/hk47bot/rotp_stfn/capability/CapabilityHandler.java
@@ -1,17 +1,14 @@
 package com.hk47bot.rotp_stfn.capability;
 
+
 import com.hk47bot.rotp_stfn.RotpStickyFingersAddon;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.nbt.INBT;
-import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.Capability.IStorage;
+import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
@@ -23,27 +20,18 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @EventBusSubscriber(modid = RotpStickyFingersAddon.MOD_ID)
 public class CapabilityHandler {
-    private static final ResourceLocation CAPABILITY_ID = new ResourceLocation(RotpStickyFingersAddon.MOD_ID, "sticky_fingers_user_data");
+    private static final ResourceLocation CAPABILITY_ID = new ResourceLocation(RotpStickyFingersAddon.MOD_ID, "sticky_fingers_storage_data");
     
     // Register our capability (this method is called from another event handler, which uses a different mod bus).
     public static void commonSetupRegister() {
-        CapabilityManager.INSTANCE.register(
-                EntityZipperData.class,
-                new IStorage<EntityZipperData>() {
-                    @Override public INBT writeNBT(Capability<EntityZipperData> capability, EntityZipperData instance, Direction side) { return instance.serializeNBT(); }
-                    @Override public void readNBT(Capability<EntityZipperData> capability, EntityZipperData instance, Direction side, INBT nbt) { instance.deserializeNBT((CompoundNBT) nbt); }
-                },
-                () -> new EntityZipperData(null));
+        CapabilityManager.INSTANCE.register(ZipperStorageCap.class, new ZipperStorageCapStorage(), () -> new ZipperStorageCap(null));
     }
 
     // Attaches the capability to all instances of LivingEntity.
     @SubscribeEvent
-    public static void onAttachCapabilitiesEntity(AttachCapabilitiesEvent<Entity> event) {
-        Entity entity = event.getObject();
-        if (entity instanceof LivingEntity) {
-            LivingEntity living = (LivingEntity) entity;
-            event.addCapability(CAPABILITY_ID, new EntityZipperDataProvider(living));
-        }
+    public static void onAttachCapabilities(AttachCapabilitiesEvent<World> event) {
+        World world = event.getObject();
+        event.addCapability(CAPABILITY_ID, new ZipperStorageCapProvider(world));
     }
 
 
@@ -53,9 +41,7 @@ public class CapabilityHandler {
         Entity entityTracked = event.getTarget();
         ServerPlayerEntity trackingPlayer = (ServerPlayerEntity) event.getPlayer();
         if (entityTracked instanceof LivingEntity) {
-            entityTracked.getCapability(EntityZipperDataProvider.CAPABILITY).ifPresent(stickyFingersUserData -> {
-                stickyFingersUserData.syncWithAnyPlayer(trackingPlayer);
-            });
+
         }
     }
 
@@ -76,9 +62,6 @@ public class CapabilityHandler {
 
     private static void syncAttachedData(PlayerEntity player) {
         ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
-        player.getCapability(EntityZipperDataProvider.CAPABILITY).ifPresent(data -> {
-            data.syncWithEntityOnly(serverPlayer);
-            data.syncWithAnyPlayer(serverPlayer);
-        });
+
     }
 }
