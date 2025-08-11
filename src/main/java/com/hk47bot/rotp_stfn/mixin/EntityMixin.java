60c31667938d338df963ead2c8e7916284406c07
Yarost228 <yarik.alferov1@gmail.com>
2025-08-11 04:49:57 +0300

    Changed the way of projectile relocation

--- src/main/java/com/hk47bot/rotp_stfn/mixin/EntityMixin.java
+++ src/main/java/com/hk47bot/rotp_stfn/mixin/EntityMixin.java
@@ -1,5 +1,6 @@
 package com.hk47bot.rotp_stfn.mixin;
 
+import com.github.standobyte.jojo.action.non_stand.VampirismSpaceRipperStingyEyes;
 import com.hk47bot.rotp_stfn.capability.EntityZipperCapability;
 import com.hk47bot.rotp_stfn.capability.EntityZipperCapabilityProvider;
 import com.hk47bot.rotp_stfn.entity.bodypart.BodyPartEntity;
@@ -15,20 +16,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class EntityMixin {
-    @Inject(method = "getEyePosition", at = @At("HEAD"), cancellable = true)
-    // FIXME: Есть проблема, что если стенд смотрит на таргет, то он смотрит на голову. А если голова в пизде находиться, то он будет смотреть куда то там анхуй
-    private void getEyePosition(float partialTicks, CallbackInfoReturnable<Vector3d> cir){
-        Entity entity = (Entity) (Object) this;
-        if (entity instanceof LivingEntity) {
-            EntityZipperCapability capability = entity.getCapability(EntityZipperCapabilityProvider.CAPABILITY).orElse(null);
-            if (capability == null) return;
+//    @Inject(method = "getEyePosition", at = @At("HEAD"), cancellable = true)
+//    // FIXME: Есть проблема, что если стенд смотрит на таргет, то он смотрит на голову. А если голова в пизде находиться, то он будет смотреть куда то там анхуй
+//    private void getEyePosition(float partialTicks, CallbackInfoReturnable<Vector3d> cir){
+//        Entity entity = (Entity) (Object) this;
+//        if (entity instanceof LivingEntity) {
+//            EntityZipperCapability capability = entity.getCapability(EntityZipperCapabilityProvider.CAPABILITY).orElse(null);
+//            if (capability == null) return;
+//
+//            if (!capability.isHasHead() && capability.getHeadId() != null) {
+//                Entity head = StickyUtil.getEntityByUUID(entity.level, capability.getHeadId());
+//                if (head instanceof PlayerHeadEntity) {
+//                    cir.setReturnValue(head.getEyePosition(partialTicks));
+//                }
+//            }
+//        }
+//    }
 
-            if (!capability.isHasHead() && capability.getHeadId() != null) {
-                Entity head = StickyUtil.getEntityByUUID(entity.level, capability.getHeadId());
-                if (head instanceof PlayerHeadEntity) {
-                    cir.setReturnValue(head.getEyePosition(partialTicks));
-                }
-            }
-        }
-    }
+    // Это ломает слишком многое, так что заменяю это на OwnerBoundProjectileEntityMixin
 }
---
+++ src/main/java/com/hk47bot/rotp_stfn/mixin/OwnerBoundProjectileEntityMixin.java
@@ -0,0 +1,59 @@
+package com.hk47bot.rotp_stfn.mixin;
+
+import com.github.standobyte.jojo.entity.damaging.DamagingEntity;
+import com.github.standobyte.jojo.entity.damaging.projectile.ownerbound.OwnerBoundProjectileEntity;
+import com.github.standobyte.jojo.util.mc.MCUtil;
+import com.hk47bot.rotp_stfn.capability.EntityZipperCapability;
+import com.hk47bot.rotp_stfn.capability.EntityZipperCapabilityProvider;
+import com.hk47bot.rotp_stfn.entity.bodypart.PlayerHeadEntity;
+import com.hk47bot.rotp_stfn.util.StickyUtil;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import javax.annotation.Nullable;
+
+@Mixin(value = OwnerBoundProjectileEntity.class, remap = false)
+public class OwnerBoundProjectileEntityMixin extends DamagingEntity {
+
+    public OwnerBoundProjectileEntityMixin(EntityType<? extends DamagingEntity> entityType, @Nullable LivingEntity owner, World world) {
+        super(entityType, owner, world);
+    }
+    @Redirect(method = "ownerPosition", at = @At(value = "INVOKE", target = "Lcom/github/standobyte/jojo/entity/damaging/projectile/ownerbound/OwnerBoundProjectileEntity;getPos(Lnet/minecraft/entity/LivingEntity;FFF)Lnet/minecraft/util/math/vector/Vector3d;"))
+    private Vector3d getEyePosition(OwnerBoundProjectileEntity instance, LivingEntity owner, float partialTick, float yRot, float xRot){
+        OwnerBoundProjectileEntity projectile = (OwnerBoundProjectileEntity) (Object) this;
+        if (owner != null) {
+            EntityZipperCapability capability = owner.getCapability(EntityZipperCapabilityProvider.CAPABILITY).orElse(null);
+            if (capability == null) return getPos(owner, partialTick, yRot, xRot);
+
+            if (!capability.isHasHead() && capability.getHeadId() != null) {
+                Entity head = StickyUtil.getEntityByUUID(owner.level, capability.getHeadId());
+                if (head instanceof PlayerHeadEntity) {
+                    return head.getEyePosition(partialTick).add(getOwnerRelativeOffset());
+                }
+            }
+        }
+        return MCUtil.getEntityPosition(projectile, partialTick);
+    }
+    @Override
+    public int ticksLifespan() {
+        return 0;
+    }
+    @Override
+    protected float getBaseDamage() {
+        return 0;
+    }
+    @Override
+    protected float getMaxHardnessBreakable() {
+        return 0;
+    }
+    @Override
+    public boolean standDamage() {
+        return false;
+    }
+}
