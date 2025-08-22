a4afc919bb1f0345fbc8d6aa9e9f2e7862591442
Weever1337 <weever1337@gmail.com>
2025-08-23 01:14:57 +0300

    fixed container issue

--- src/main/java/com/hk47bot/rotp_stfn/container/BlockZipperContainer.java
+++ src/main/java/com/hk47bot/rotp_stfn/container/BlockZipperContainer.java
@@ -2,7 +2,6 @@ package com.hk47bot.rotp_stfn.container;
 
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 import com.hk47bot.rotp_stfn.capability.BlockZipperStorage;
-import com.hk47bot.rotp_stfn.capability.EntityZipperStorage;
 import com.hk47bot.rotp_stfn.init.InitContainers;
 import com.hk47bot.rotp_stfn.init.InitStands;
 import net.minecraft.entity.player.PlayerEntity;
@@ -15,10 +14,10 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.server.ServerWorld;
 
-import java.util.UUID;
-
 public class BlockZipperContainer extends Container {
     private final BlockZipperStorage storage;
+    private static final int PLAYER_INV_SLOT_COUNT = 36;
+    private static final int ZIPPER_INV_SLOT_COUNT = 18;
 
     public BlockZipperContainer(int id, PlayerInventory inventory, PacketBuffer dataFromServer) {
         this(id, inventory, new BlockZipperStorage(new BlockPos(0, 0, 0), StringTextComponent.EMPTY));
@@ -27,22 +26,22 @@ public class BlockZipperContainer extends Container {
     public BlockZipperContainer(int id, PlayerInventory inventory, BlockZipperStorage itemCap) {
         super(InitContainers.STICKY_FINGERS_BLOCK_CONTAINER.get(), id);
         this.storage = itemCap;
+
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 9; ++j) {
-                addSlot(new Slot(inventory, j + i * 9 + 9,  8 + j * 18,    i * 18 + 67));
+                this.addSlot(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, i * 18 + 67));
             }
         }
 
         for (int k = 0; k < 9; ++k) {
-            addSlot(new Slot(inventory, k, 8 + k * 18, 125));
+            this.addSlot(new Slot(inventory, k, 8 + k * 18, 125));
         }
 
         for (int i = 0; i < 2; ++i) {
             for (int j = 0; j < 9; ++j) {
-                addSlot(new Slot(itemCap, j + i * 9,  8 + j * 18,    (i+1) * 18));
+                this.addSlot(new Slot(itemCap, j + i * 9, 8 + j * 18, (i + 1) * 18));
             }
         }
-
     }
 
     @Override
@@ -57,43 +56,34 @@ public class BlockZipperContainer extends Container {
         ItemStack itemstack = ItemStack.EMPTY;
         Slot slot = this.slots.get(index);
         if (slot != null && slot.hasItem()) {
-            ItemStack itemstack1 = slot.getItem();
-            itemstack = itemstack1.copy();
-            if (index == 0) {
-                if (!this.moveItemStackTo(itemstack1, 1, 37, true)) {
-                    return ItemStack.EMPTY;
-                }
+            ItemStack sourceStack = slot.getItem();
+            itemstack = sourceStack.copy();
 
-                slot.onQuickCraft(itemstack1, itemstack);
-            } else if (this.moveItemStackTo(itemstack1, 0, 1, false)) {
-                return ItemStack.EMPTY;
-            } else if (index >= 1 && index < 28) {
-                if (!this.moveItemStackTo(itemstack1, 28, 37, false)) {
+            if (index < PLAYER_INV_SLOT_COUNT) {
+                if (!this.moveItemStackTo(sourceStack, PLAYER_INV_SLOT_COUNT, PLAYER_INV_SLOT_COUNT + ZIPPER_INV_SLOT_COUNT, false)) {
                     return ItemStack.EMPTY;
                 }
-            } else if (index >= 28 && index < 37) {
-                if (!this.moveItemStackTo(itemstack1, 1, 28, false)) {
+            } else if (index < PLAYER_INV_SLOT_COUNT + ZIPPER_INV_SLOT_COUNT) {
+                if (!this.moveItemStackTo(sourceStack, 0, PLAYER_INV_SLOT_COUNT, false)) {
                     return ItemStack.EMPTY;
                 }
-            } else if (!this.moveItemStackTo(itemstack1, 1, 37, false)) {
+            } else {
                 return ItemStack.EMPTY;
             }
 
-            if (itemstack1.isEmpty()) {
+            if (sourceStack.isEmpty()) {
                 slot.set(ItemStack.EMPTY);
             } else {
                 slot.setChanged();
             }
 
-            if (itemstack1.getCount() == itemstack.getCount()) {
+            if (sourceStack.getCount() == itemstack.getCount()) {
                 return ItemStack.EMPTY;
             }
 
-            slot.onTake(player, itemstack1);
+            slot.onTake(player, sourceStack);
         }
 
         return itemstack;
     }
-
 }
-
--- src/main/java/com/hk47bot/rotp_stfn/container/EntityZipperContainer.java
+++ src/main/java/com/hk47bot/rotp_stfn/container/EntityZipperContainer.java
@@ -1,12 +1,9 @@
 package com.hk47bot.rotp_stfn.container;
 
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
-import com.hk47bot.rotp_stfn.capability.BlockZipperStorage;
 import com.hk47bot.rotp_stfn.capability.EntityZipperStorage;
 import com.hk47bot.rotp_stfn.init.InitContainers;
 import com.hk47bot.rotp_stfn.init.InitStands;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.Container;
@@ -14,34 +11,37 @@ import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
 import java.util.UUID;
 
 public class EntityZipperContainer extends Container {
 
-    private EntityZipperStorage storage;
+    private final EntityZipperStorage storage;
+    private static final int PLAYER_INV_SLOT_COUNT = 36;
+    private static final int ZIPPER_INV_SLOT_COUNT = 9;
 
     public EntityZipperContainer(int id, PlayerInventory inventory, PacketBuffer dataFromServer) {
         this(id, inventory, new EntityZipperStorage(StringTextComponent.EMPTY, UUID.randomUUID()));
     }
+
     public EntityZipperContainer(int id, PlayerInventory inventory, EntityZipperStorage itemCap) {
         super(InitContainers.STICKY_FINGERS_ENTITY_CONTAINER.get(), id);
         this.storage = itemCap;
+
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 9; ++j) {
-                addSlot(new Slot(inventory, j + i * 9 + 9,  8 + j * 18,    i * 18 + 84));
+                this.addSlot(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, i * 18 + 84));
             }
         }
 
         for (int k = 0; k < 9; ++k) {
-            addSlot(new Slot(inventory, k, 8 + k * 18, 142));
+            this.addSlot(new Slot(inventory, k, 8 + k * 18, 142));
         }
 
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 3; ++j) {
-                addSlot(new Slot(itemCap, j + i * 3,  62 + j * 18,    i * 18 + 17));
+                this.addSlot(new Slot(itemCap, j + i * 3, 62 + j * 18, i * 18 + 17));
             }
         }
     }
@@ -51,39 +51,32 @@ public class EntityZipperContainer extends Container {
         ItemStack itemstack = ItemStack.EMPTY;
         Slot slot = this.slots.get(index);
         if (slot != null && slot.hasItem()) {
-            ItemStack itemstack1 = slot.getItem();
-            itemstack = itemstack1.copy();
-            if (index == 0) {
-                if (!this.moveItemStackTo(itemstack1, 1, 37, true)) {
-                    return ItemStack.EMPTY;
-                }
+            ItemStack sourceStack = slot.getItem();
+            itemstack = sourceStack.copy();
 
-                slot.onQuickCraft(itemstack1, itemstack);
-            } else if (this.moveItemStackTo(itemstack1, 0, 1, false)) {
-                return ItemStack.EMPTY;
-            } else if (index >= 1 && index < 28) {
-                if (!this.moveItemStackTo(itemstack1, 28, 37, false)) {
+            if (index < PLAYER_INV_SLOT_COUNT) {
+                if (!this.moveItemStackTo(sourceStack, PLAYER_INV_SLOT_COUNT, PLAYER_INV_SLOT_COUNT + ZIPPER_INV_SLOT_COUNT, false)) {
                     return ItemStack.EMPTY;
                 }
-            } else if (index >= 28 && index < 37) {
-                if (!this.moveItemStackTo(itemstack1, 1, 28, false)) {
+            } else if (index < PLAYER_INV_SLOT_COUNT + ZIPPER_INV_SLOT_COUNT) {
+                if (!this.moveItemStackTo(sourceStack, 0, PLAYER_INV_SLOT_COUNT, false)) {
                     return ItemStack.EMPTY;
                 }
-            } else if (!this.moveItemStackTo(itemstack1, 1, 37, false)) {
+            } else {
                 return ItemStack.EMPTY;
             }
 
-            if (itemstack1.isEmpty()) {
+            if (sourceStack.isEmpty()) {
                 slot.set(ItemStack.EMPTY);
             } else {
                 slot.setChanged();
             }
 
-            if (itemstack1.getCount() == itemstack.getCount()) {
+            if (sourceStack.getCount() == itemstack.getCount()) {
                 return ItemStack.EMPTY;
             }
 
-            slot.onTake(player, itemstack1);
+            slot.onTake(player, sourceStack);
         }
 
         return itemstack;
@@ -92,7 +85,8 @@ public class EntityZipperContainer extends Container {
     @Override
     public boolean stillValid(PlayerEntity player) {
         IStandPower power = IStandPower.getPlayerStandPower(player);
+        if (player.level.isClientSide()) return true;
         ServerWorld world = (ServerWorld) player.level;
-        return power.getType() == InitStands.STAND_STICKY_FINGERS.getStandType() && world.getEntity(storage.getMobUUID()).isAlive();
+        return power.getType() == InitStands.STAND_STICKY_FINGERS.getStandType() && world.getEntity(storage.getMobUUID()) != null && world.getEntity(storage.getMobUUID()).isAlive();
     }
 }
